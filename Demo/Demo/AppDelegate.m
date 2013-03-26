0fe4bb8ab2bd79cab328efebd10728a7c5c1b7a2
zhangxi <zhangxi_1989@sina.com>
2013-03-26 13:54:47 +0800

    change name ZXMacro

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ZXMicro.h"
+#import "ZXMacro.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
